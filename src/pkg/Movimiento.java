b94147cb3b8d328960c0d844c506c84c5ba9242c
Jesús <Jess@DESKTOP-C99UU8S>
2023-10-11 15:13:36 +0200

    Creo la clase Movimiento

--- src/pkg/Movimiento.java
+++ src/pkg/Movimiento.java
@@ -2,4 +2,38 @@ package pkg;
 
 public class Movimiento {
 
+	public enum signo {D, H};
+
+	private double importe;
+	private String detalle;
+	private signo sig;
+
+	public Movimiento(double importe, signo sig) {
+		this.importe = importe;
+		this.sig = sig;
+	}
+
+	public double getImporte() {
+		return importe;
+	}
+
+	public void setImporte(double importe) {
+		this.importe = importe;
+	}
+
+	public String getDetalle() {
+		return detalle;
+	}
+
+	public void setDetalle(String detalle) {
+		this.detalle = detalle;
+	}
+
+	public signo getSig() {
+		return sig;
+	}
+
+	public void setSig(signo sig) {
+		this.sig = sig;
+	}
 }
